c6a2978151869ac612a0a18409e4af3a0e36935f
Jorge L Pérez <jolisper@gmail.com>
2014-05-06 03:58:27 -0300

    Deleted unnecesary files and some refactor on move strategies file

--- lib/ruby_chess/move_strategies/move_strategy.rb
+++ lib/ruby_chess/move_strategies/move_strategy.rb
@@ -23,30 +23,4 @@ module RubyChess
 
   end
 
-  class KingMoveStrategy < MoveStrategy
-    
-    def initialize
-    end
-
-    def valid_moves(piece, square)
-      directions = ['top_left', 'top', 'top_right', 'right', 
-                    'bottom_right', 'bottom', 'bottom_left', 'left']
-
-      valid_moves = []
-
-      directions.each do |direction|
-        next_square = square.send("#{direction}")
-        if next_square 
-          if next_square.empty?
-            valid_moves << Move.make_a_simple_move(square, next_square)
-          elsif top_square.has_a_piece_of_the_opposite_color piece
-            valid_moves << Move.make_a_capture_move(square, next_square)
-          end
-        end
-      end
-
-      valid_moves
-    end
-  end
-
 end
